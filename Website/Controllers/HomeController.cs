824798364620ec8e3394d5a240be8e84fec0d044
Connor Waiter <connor.waiter.2022@uni.strath.ac.uk>
2024-04-03 22:36:20 +0100

    Themed and published

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -2,14 +2,9 @@
 
 namespace Website.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger) : Controller
     {
-        private readonly ILogger _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger _logger = logger;
 
         [Route("")]
         public IActionResult Home()
